76bb10b4aaed5e0e7ffa3786df6491a276158adc
Kazuki Ota <kaota@microsoft.com>
2021-04-22 17:00:45 +0900

    add

--- DotnetLab.Demo02.Api/Greet.cs
+++ DotnetLab.Demo02.Api/Greet.cs
@@ -16,18 +16,13 @@ namespace DotnetLab.Demo02.Api
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            var principal = StaticWebAppsAuth.Parse(req);
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
+            var name = principal.Identity.IsAuthenticated ?
+                principal.Identity.Name :
+                "Anonymous";
+            var responseMessage = $"Hello, {name}. This HTTP triggered function executed successfully.";
             return new OkObjectResult(responseMessage);
         }
     }
